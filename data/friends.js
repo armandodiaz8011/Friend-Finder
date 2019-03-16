968f70795c110b0c0ee02aaf5777a57fe0f8a2d3
Armando Diaz <armandodiaz8011@Armandos-MacBook-Pro.local>
2019-03-16 14:08:08 -0400

    homework

---
+++ data/friends.js
@@ -0,0 +1,83 @@
+var surveyArray = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+        },
+        {
+        "name": "Beethoven",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Beethoven.jpg/220px-Beethoven.jpg",
+        "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+        ]
+        },
+        {
+        "name": "Mozart",
+        "photo": "https://cdn.britannica.com/s:300x300/50/152550-004-669D4462.jpg",
+        "scores": [
+        "1",
+        "2",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Bach",
+        "photo": "https://cdn.britannica.com/s:300x300/61/114461-004-55247C77.jpg",
+        "scores": [
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1",
+        "1"
+        ]
+        },
+        {
+        "name": "Tchaikovsky",
+        "photo": "https://cdn.britannica.com/s:290x800/29/70029-004-74AD88C3.jpg",
+        "scores": [
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2"
+        ]
+        }
+];
+module.exports=surveyArray
